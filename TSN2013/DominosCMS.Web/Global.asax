0999cf3fbda151383c839aa57ba8abca6ea87d87
Rami Mohsen <itian.remo@gmail.com>
2015-03-31 19:31:49 +0200

    clear the project folder

--- TSN2013/DominosCMS.Web/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DominosCMS.Web.MvcApplication" Language="C#" %>
--- TSN2013/DominosCMS.Web/_static/Content/HW/HW-D-0280_files/copyrightOCD_2005.js
+++
@@ -1,1 +0,0 @@
-document.write('<a href="http://www.ul.com/copyrite.htm">Copyright &#169; 2007 Underwriters Laboratories Inc.</a>');
